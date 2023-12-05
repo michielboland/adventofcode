4ef54e0f9666d284ef7e25d6d1d016477c38b874
Michiel Boland <michiel@boland.org>
2023-12-05 17:56:48 +0100

    Day 5 puzzle 1

--- src/main/java/day5/Day5.java
+++ src/main/java/day5/Day5.java
@@ -4,13 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+enum Category {
+    SEED, SOIL, FERTILIZER, WATER, LIGHT, TEMPERATURE, HUMIDITY, LOCATION;
+
+    static Category from(String name) {
+        return Arrays.stream(values()).filter(c -> c.name().toLowerCase().equals(name)).findFirst().orElseThrow();
+    }
+}
 
 record MapEntry(long destStart, long sourceStart, long length) {
     private static final Pattern DIGITS = Pattern.compile("\\s*(\\d+)\\s+(\\d+)\\s+(\\d+)\\s*");
@@ -22,11 +34,23 @@ record MapEntry(long destStart, long sourceStart, long length) {
         }
         return new MapEntry(Long.parseLong(matcher.group(1)), Long.parseLong(matcher.group(2)), Long.parseLong(matcher.group(3)));
     }
+
+    Optional<MapEntry> matches(long l) {
+        return l >= sourceStart && l < sourceStart + length ? Optional.of(this) : Optional.empty();
+    }
+
+    long remap(long l) {
+        return l - sourceStart + destStart;
+    }
 }
 
-record TMap(String name, List<MapEntry> entries) {
-    TMap(String name) {
-        this(name, new ArrayList<>());
+record TMap(Category from, Category to, List<MapEntry> entries) {
+    TMap(String fromName, String toName) {
+        this(Category.from(fromName), Category.from(toName), new ArrayList<>());
+    }
+
+    long map(long l) {
+        return entries.stream().map(e -> e.matches(l)).filter(Optional::isPresent).map(Optional::get).findFirst().map(e -> e.remap(l)).orElse(l);
     }
 }
 
@@ -39,11 +63,18 @@ public class Day5 {
 class LocationFinder {
     private static final Pattern HEADER = Pattern.compile("seeds: (.*)");
     private static final Pattern DIGIT = Pattern.compile("(\\d+)");
-    private static final Pattern MAP = Pattern.compile("(.*) map:");
+    private static final Pattern MAP = Pattern.compile("(.*)-to-(.*) map:");
     private final Set<Long> seeds = new HashSet<>();
-    private final List<TMap> maps = new ArrayList<>();
+    private final Map<Category, TMap> maps = new TreeMap<>();
     TMap tMap;
     State state = State.INIT;
+
+    long location(long seed) {
+        final Map<Category, Long> values = new TreeMap<>();
+        values.put(Category.SEED, seed);
+        Arrays.stream(Category.values()).filter(c -> c != Category.LOCATION).forEachOrdered(c -> values.put(maps.get(c).to(), maps.get(c).map(values.get(maps.get(c).from()))));
+        return values.get(Category.LOCATION);
+    }
 
     void parse(String line) {
         switch (state) {
@@ -72,8 +103,8 @@ class LocationFinder {
         if (!matcher.matches()) {
             throw new IllegalArgumentException();
         }
-        tMap = new TMap(matcher.group(1));
-        maps.add(tMap);
+        tMap = new TMap(matcher.group(1), matcher.group(2));
+        maps.put(tMap.from(), tMap);
         state = State.DIGITS;
     }
 
@@ -86,7 +117,7 @@ class LocationFinder {
     }
 
     void solve() {
-        System.out.println(maps);
+        System.out.println(seeds.stream().map(this::location).mapToLong(a -> a).min().orElseThrow());
     }
 
     enum State {INIT, BLANK, DIGITS}
