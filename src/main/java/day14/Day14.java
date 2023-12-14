32b0c7063034b202d6f0800adafe5c5e55e2e95e
Michiel Boland <michiel@boland.org>
2023-12-14 23:46:05 +0100

    Day 14 puzzle 2

--- src/main/java/day14/Day14.java
+++ src/main/java/day14/Day14.java
@@ -3,13 +3,21 @@ package day14;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 enum Rock {
@@ -48,6 +56,10 @@ record Coordinate(int x, int y) implements Comparable<Coordinate> {
         return new Coordinate(x + 1, y);
     }
 
+    boolean inside(int width, int height) {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
     @Override
     public int compareTo(Coordinate o) {
         return y == o.y ? Integer.compare(x, o.x) : Integer.compare(y, o.y);
@@ -79,8 +91,8 @@ record Platform(Map<Coordinate, Rock> rocks, int width, int height) {
         return new Platform(rocks, width, height);
     }
 
-    void tiltColumnNorth(int x) {
-        for (Coordinate a = new Coordinate(x, 0), b = a; a.y() < height; a = a.south()) {
+    void tilt(Supplier<Coordinate> s, Function<Coordinate, Coordinate> f) {
+        for (Coordinate a = s.get(), b = a; a.inside(width, height); a = f.apply(a)) {
             var rock = rocks.get(a);
             if (rock != null) {
                 switch (rock) {
@@ -89,22 +101,48 @@ record Platform(Map<Coordinate, Rock> rocks, int width, int height) {
                             rocks.remove(a);
                             rocks.put(b, rock);
                         }
-                        b = b.south();
+                        b = f.apply(b);
                     }
-                    case CUBE -> b = a.south();
+                    case CUBE -> b = f.apply(a);
                 }
             }
         }
     }
 
     void tiltNorth() {
-        for (int x = 0; x < width; x++) {
-            tiltColumnNorth(x);
-        }
+        IntStream.range(0, width).forEach(x -> tilt(() -> new Coordinate(x, 0), Coordinate::south));
+    }
+
+    void tiltWest() {
+        IntStream.range(0, height).forEach(y -> tilt(() -> new Coordinate(0, y), Coordinate::east));
+    }
+
+    void tiltSouth() {
+        IntStream.range(0, width).forEach(x -> tilt(() -> new Coordinate(x, height - 1), Coordinate::north));
+    }
+
+    void tiltEast() {
+        IntStream.range(0, width).forEach(y -> tilt(() -> new Coordinate(width - 1, y), Coordinate::west));
+    }
+
+    void cycle() {
+        tiltNorth();
+        tiltWest();
+        tiltSouth();
+        tiltEast();
     }
 
     int load() {
         return rocks.entrySet().stream().filter(e -> e.getValue() == Rock.ROUND).mapToInt(e -> height - e.getKey().y()).sum();
+    }
+
+    Rock at(int x, int y) {
+        return Optional.ofNullable(rocks.get(new Coordinate(x, y))).orElse(Rock.SPACE);
+    }
+
+    @Override
+    public String toString() {
+        return IntStream.range(0, height).mapToObj(y -> IntStream.range(0, width).mapToObj(x -> String.valueOf(at(x, y).label)).collect(Collectors.joining())).collect(Collectors.joining("\n"));
     }
 }
 
@@ -115,6 +153,16 @@ class Puzzle {
             var platform = Platform.parse(reader.lines());
             platform.tiltNorth();
             System.out.println(platform.load());
+            List<String> l = new ArrayList<>();
+            List<Integer> l2 = new ArrayList<>();
+            do {
+                platform.cycle();
+                l.add(platform.toString());
+                l2.add(platform.load());
+            } while (l.size() == new HashSet<>(l).size());
+            int last = l.size() - 1;
+            int first = l.indexOf(l.get(last));
+            System.out.println(l2.get(first + (1000000000 - 1 - first) % (last - first)));
         }
     }
 }
