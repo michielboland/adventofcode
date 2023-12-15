002f7050408e5f79e0aededd1b547029293232bc
Michiel Boland <michiel@boland.org>
2023-12-15 21:07:14 +0100

    Alternate Day 12 implementation (should still suck)

--- src/main/java/day12/Day12.java
+++ src/main/java/day12/Day12.java
@@ -3,13 +3,13 @@ package day12;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -24,131 +24,136 @@ enum Condition {
     static Condition from(int symbol) {
         return Arrays.stream(values()).filter(c -> c.symbol == symbol).findFirst().orElseThrow();
     }
-
-    List<Condition> repeat(int n) {
-        return IntStream.range(0, n).mapToObj(z -> this).toList();
-    }
-
-    boolean matches(Condition other) {
-        return this == other || this == UNKNOWN || other == UNKNOWN;
-    }
 }
 
 record SpringConditions(List<Condition> conditions) {
-    static SpringConditions from(String line) {
-        return new SpringConditions(line.chars().mapToObj(Condition::from).toList());
+    static SpringConditions from(String line, int n) {
+        var dup = IntStream.range(0, n).mapToObj(z -> line).collect(Collectors.joining("?"));
+        return new SpringConditions(dup.chars().mapToObj(Condition::from).toList());
     }
 
-    boolean matches(SpringConditions other) {
-        if (conditions.size() != other.conditions.size()) {
-            throw new IllegalArgumentException();
+    boolean matches(int start, int end) {
+        int p = start;
+        if (p > 0 && conditions.get(p - 1) == Condition.DAMAGED) {
+            return false;
         }
-        return IntStream.range(0, conditions.size()).allMatch(i -> conditions.get(i).matches(other.conditions.get(i)));
+        while (p < end) {
+            if (conditions.get(p++) == Condition.OPERATIONAL) {
+                return false;
+            }
+        }
+        return p >= conditions.size() || conditions.get(p) != Condition.DAMAGED;
+    }
+
+    boolean matches(List<Interval> intervals) {
+        int p = 0;
+        for (var d : intervals) {
+            while (p < d.start()) {
+                if (conditions.get(p++) == Condition.DAMAGED) {
+                    return false;
+                }
+            }
+            while (p < d.end()) {
+                if (conditions.get(p++) == Condition.OPERATIONAL) {
+                    return false;
+                }
+            }
+        }
+        while (p < conditions.size()) {
+            if (conditions.get(p++) == Condition.DAMAGED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return conditions.stream().map(c -> String.valueOf(c.symbol)).collect(Collectors.joining());
     }
 }
 
+record Interval(int start, int end) {
+}
+
 record GroupSizesOfDamagedSprings(List<Integer> sizes) {
-    static GroupSizesOfDamagedSprings from(String line) {
-        return new GroupSizesOfDamagedSprings(Arrays.stream(line.split(",")).map(Integer::parseInt).toList());
+    static GroupSizesOfDamagedSprings from(String line, int n) {
+        var dup = IntStream.range(0, n).mapToObj(z -> line).collect(Collectors.joining(","));
+        return new GroupSizesOfDamagedSprings(Arrays.stream(dup.split(",")).map(Integer::parseInt).toList());
     }
 
-    SpringConditions weave(List<Integer> otherSizes, boolean inside) {
-        Iterator<Integer> i = sizes.iterator();
-        Iterator<Integer> j = otherSizes.iterator();
-        List<Condition> conditions = new ArrayList<>();
-        do {
-            if (inside) {
-                if (i.hasNext()) {
-                    conditions.addAll(Condition.DAMAGED.repeat(i.next()));
-                }
-                if (j.hasNext()) {
-                    conditions.addAll(Condition.OPERATIONAL.repeat(j.next()));
-                }
-            } else {
-                if (j.hasNext()) {
-                    conditions.addAll(Condition.OPERATIONAL.repeat(j.next()));
-                }
-                if (i.hasNext()) {
-                    conditions.addAll(Condition.DAMAGED.repeat(i.next()));
-                }
-            }
-        } while (i.hasNext() || j.hasNext());
-        return new SpringConditions(conditions);
+    @Override
+    public String toString() {
+        return sizes.stream().map(String::valueOf).collect(Collectors.joining(","));
     }
 }
 
 record ConditionRecord(SpringConditions springConditions, GroupSizesOfDamagedSprings groupSizesOfDamagedSprings) {
-    static final Partitioner PARTITIONER = new Partitioner();
-
-    static ConditionRecord from(String line) {
+    static ConditionRecord from(String line, int n) {
         var parts = line.split(" ");
-        return new ConditionRecord(SpringConditions.from(parts[0]), GroupSizesOfDamagedSprings.from(parts[1]));
+        return new ConditionRecord(SpringConditions.from(parts[0], n), GroupSizesOfDamagedSprings.from(parts[1], n));
     }
 
-    long arrangements() {
-        var groups = groupSizesOfDamagedSprings.sizes().size();
-        var undamagedSprings = springConditions.conditions().size() - groupSizesOfDamagedSprings.sizes().stream().mapToInt(i -> i).sum();
-        List<SpringConditions> toTest = new ArrayList<>();
-        var inside = PARTITIONER.orderedPartitionsOfSize(groups - 1, undamagedSprings);
-        var comb = PARTITIONER.orderedPartitionsOfSize(groups, undamagedSprings);
-        var outside = PARTITIONER.orderedPartitionsOfSize(groups + 1, undamagedSprings);
-        toTest.addAll(inside.stream().map(l -> groupSizesOfDamagedSprings.weave(l, true)).toList());
-        toTest.addAll(comb.stream().map(l -> groupSizesOfDamagedSprings.weave(l, true)).toList());
-        toTest.addAll(comb.stream().map(l -> groupSizesOfDamagedSprings.weave(l, false)).toList());
-        toTest.addAll(outside.stream().map(l -> groupSizesOfDamagedSprings.weave(l, false)).toList());
-        return toTest.stream().filter(s -> s.matches(springConditions)).count();
+    private static <T> List<T> concat(List<T> a, List<T> b) {
+        var l = new ArrayList<>(a);
+        l.addAll(b);
+        return l;
+    }
+
+    BigInteger arrangements() {
+        return arrangements(0, -1, Collections.emptyList());
+    }
+
+    BigInteger arrangements(int g, int start, List<Interval> intervals) {
+        BigInteger r;
+        if (g == groupSizesOfDamagedSprings.sizes().size()) {
+            if (springConditions.matches(intervals)) {
+                r = BigInteger.ONE;
+            } else {
+                r = BigInteger.ZERO;
+            }
+        } else {
+            int groupSize = groupSizesOfDamagedSprings.sizes().get(g);
+            int offset = IntStream.range(0, g).map(gg -> groupSizesOfDamagedSprings.sizes().get(gg)).sum() - 1;
+            r = BigInteger.ZERO;
+            for (int i = Integer.max(start, offset); i + groupSize < springConditions.conditions().size(); i++) {
+                if (springConditions.matches(i + 1, i + groupSize + 1)) {
+                    BigInteger a = arrangements(g + 1, i + groupSize + 1, concat(intervals, List.of(new Interval(i + 1, i + groupSize + 1))));
+                    r = r.add(a);
+                }
+            }
+        }
+        return r;
+    }
+
+    @Override
+    public String toString() {
+        return springConditions + " " + groupSizesOfDamagedSprings;
     }
 }
 
 record Springs(List<ConditionRecord> conditionRecords) {
-    static Springs parse(Stream<String> lines) {
-        return new Springs(lines.map(ConditionRecord::from).toList());
+    static Springs parse(Stream<String> lines, int n) {
+        return new Springs(lines.map(line -> ConditionRecord.from(line, n)).toList());
     }
 
-    long arrangements() {
-        return conditionRecords.stream().mapToLong(ConditionRecord::arrangements).sum();
+    BigInteger arrangements() {
+        return conditionRecords.stream().map(ConditionRecord::arrangements).reduce(BigInteger.ZERO, BigInteger::add);
     }
 }
 
 class Puzzle {
-    void solve() throws IOException {
+    void solve(@SuppressWarnings("SameParameterValue") int n) throws IOException {
         try (var input = Objects.requireNonNull(getClass().getResourceAsStream("/day12/day12_input"))) {
             var reader = new BufferedReader(new InputStreamReader(input));
-            var springs = Springs.parse(reader.lines());
+            var springs = Springs.parse(reader.lines(), n);
             System.out.println(springs.arrangements());
         }
     }
 }
 
-class Partitioner {
-    private final Map<NT, List<List<Integer>>> CACHE = new HashMap<>();
-
-    List<Integer> append(List<Integer> other, int n) {
-        return Stream.concat(other.stream(), Stream.of(n)).toList();
-    }
-
-    List<List<Integer>> orderedPartitionsOfSize(int numberOfElements, int total) {
-        NT key = new NT(numberOfElements, total);
-        if (CACHE.containsKey(key)) {
-            return CACHE.get(key);
-        }
-        if (numberOfElements < 1) {
-            throw new IllegalArgumentException();
-        }
-        if (numberOfElements == 1) {
-            return List.of(List.of(total));
-        }
-        var r = IntStream.range(1, total).boxed().flatMap(n -> orderedPartitionsOfSize(numberOfElements - 1, total - n).stream().map(l -> append(l, n))).toList();
-        CACHE.put(key, r);
-        return r;
-    }
-
-    record NT(int numberOfElements, int total) {
-    }
-}
-
 public class Day12 {
     public static void main(String[] args) throws IOException {
-        new Puzzle().solve();
+        new Puzzle().solve(1);
     }
 }
