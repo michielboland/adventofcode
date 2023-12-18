0b24bbffd9b40e44700530d017c1db50ee692289
Michiel Boland <michiel@boland.org>
2023-12-18 18:53:35 +0100

    Day 18 puzzle 2

--- src/main/java/day18/Day18.java
+++ src/main/java/day18/Day18.java
@@ -1,6 +1,5 @@
 package day18;
 
-import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,54 +8,73 @@ import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 import java.util.regex.Pattern;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 enum Heading {
-    NORTH(Coordinate::north), SOUTH(Coordinate::south), EAST(Coordinate::east), WEST(Coordinate::west);
-    final Function<Coordinate, Coordinate> nextFunction;
+    EAST(Coordinate::east), SOUTH(Coordinate::south), WEST(Coordinate::west), NORTH(Coordinate::north);
+    final Mover<Coordinate> nextFunction;
 
-    Heading(Function<Coordinate, Coordinate> nextFunction) {
+    Heading(Mover<Coordinate> nextFunction) {
         this.nextFunction = nextFunction;
     }
 
     static Heading from(int c) {
         return switch (c) {
-            case 'U' -> NORTH;
-            case 'D' -> SOUTH;
-            case 'L' -> WEST;
-            case 'R' -> EAST;
+            case '0', 'R' -> EAST;
+            case '1', 'D' -> SOUTH;
+            case '2', 'L' -> WEST;
+            case '3', 'U' -> NORTH;
             default -> throw new IllegalArgumentException();
         };
     }
 }
 
-public class Day18 {
-    public static void main(String[] args) throws IOException {
-        new Puzzle().solve();
+enum Turn {
+    CLOCKWISE, COUNTERCLOCKWISE;
+
+    static Turn from(Heading from, Heading to) {
+        int d = to.ordinal() - from.ordinal() & 0x3;
+        return switch (d) {
+            case 1 -> CLOCKWISE;
+            case 3 -> COUNTERCLOCKWISE;
+            default -> throw new IllegalStateException();
+        };
     }
 }
 
-record Coordinate(int x, int y) implements Comparable<Coordinate> {
-    Coordinate north() {
-        return new Coordinate(x, y - 1);
+@FunctionalInterface
+interface Mover<T> {
+    T move(T movable, long units);
+}
+
+public class Day18 {
+    public static void main(String[] args) throws IOException {
+        new Puzzle().solve(false);
+        new Puzzle().solve(true);
+    }
+}
+
+record Coordinate(long x, long y) implements Comparable<Coordinate> {
+    Coordinate north(long units) {
+        return new Coordinate(x, y - units);
     }
 
-    Coordinate south() {
-        return new Coordinate(x, y + 1);
+    Coordinate south(long units) {
+        return new Coordinate(x, y + units);
     }
 
-    Coordinate west() {
-        return new Coordinate(x - 1, y);
+    Coordinate west(long units) {
+        return new Coordinate(x - units, y);
     }
 
-    Coordinate east() {
-        return new Coordinate(x + 1, y);
+    Coordinate east(long units) {
+        return new Coordinate(x + units, y);
     }
 
     @Override
     public int compareTo(Coordinate o) {
-        return y == o.y ? Integer.compare(x, o.x) : Integer.compare(y, o.y);
+        return y == o.y ? Long.compare(x, o.x) : Long.compare(y, o.y);
     }
 
     @Override
@@ -65,64 +83,80 @@ record Coordinate(int x, int y) implements Comparable<Coordinate> {
     }
 }
 
-record Instruction(Heading heading, int meters, Color color) {
-    static final Pattern PATTERN = Pattern.compile("(.) (\\d+) \\(#(.*)\\)");
+record Instruction(Heading heading, long meters) {
+    static final Pattern PATTERN = Pattern.compile("(.) (\\d+) \\(#(.*)(.)\\)");
 
-    static Instruction from(String line) {
+    static Instruction from(String line, boolean correct) {
         var m = PATTERN.matcher(line);
         if (!m.matches()) {
             throw new IllegalArgumentException();
         }
-        return new Instruction(Heading.from(m.group(1).codePointAt(0)), Integer.parseInt(m.group(2)), Color.decode("0x" + m.group(3)));
+        if (correct) {
+            return new Instruction(Heading.from(m.group(4).codePointAt(0)), Long.parseLong(m.group(3), 16));
+        } else {
+            return new Instruction(Heading.from(m.group(1).codePointAt(0)), Integer.parseInt(m.group(2)));
+        }
     }
 }
 
 class Digger {
     Coordinate coordinate = new Coordinate(0, 0);
 
-    Coordinate head(Heading heading) {
-        coordinate = heading.nextFunction.apply(coordinate);
+    Coordinate follow(Instruction instruction) {
+        coordinate = instruction.heading().nextFunction.move(coordinate, instruction.meters());
         return coordinate;
-    }
-
-    Stream<Coordinate> follow(Instruction instruction) {
-        return IntStream.range(0, instruction.meters()).mapToObj(i -> head(instruction.heading()));
     }
 }
 
 record DigPlan(List<Instruction> instructions) {
-    static DigPlan parse(Stream<String> lines) {
-        return new DigPlan(lines.map(Instruction::from).toList());
+    static DigPlan parse(Stream<String> lines, boolean correct) {
+        return new DigPlan(lines.map(line -> Instruction.from(line, correct)).toList());
     }
 
-    List<Coordinate> coordinates() {
-        var digger = new Digger();
-        return instructions.stream().flatMap(digger::follow).toList();
-    }
-
-    // copied from day 10
-    static int countInteriorPoints(List<Coordinate> path) {
+    static long area(List<Coordinate> path) {
         List<Coordinate> loop = new ArrayList<>(path);
         loop.add(path.get(0));
-        int doubleArea = 0;
+        long doubleArea = 0;
+        //noinspection DuplicatedCode - copied from day 10
         for (int i = 0; i + 1 < loop.size(); i++) {
             var a = loop.get(i);
             var b = loop.get(i + 1);
             doubleArea += a.x() * b.y() - a.y() * b.x();
         }
-        return (Math.abs(doubleArea) - path.size()) / 2 + 1;
+        return Math.abs(doubleArea) / 2;
     }
 
-    int execute() {
-        return coordinates().size() + countInteriorPoints(coordinates());
+    List<Coordinate> coordinates() {
+        var digger = new Digger();
+        return instructions.stream().map(digger::follow).toList();
+    }
+
+    List<Turn> turns() {
+        List<Turn> turns = new ArrayList<>();
+        var lastHeading = instructions.get(instructions.size() - 1).heading();
+        for (Instruction instruction : instructions) {
+            var heading = instruction.heading();
+            turns.add(Turn.from(lastHeading, heading));
+            lastHeading = heading;
+        }
+        return turns;
+    }
+
+    long execute() {
+        var map = turns().stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        var totalCounterclockwise = map.getOrDefault(Turn.COUNTERCLOCKWISE, 0L);
+        var totalClockwise = map.getOrDefault(Turn.CLOCKWISE, 0L);
+        long corners = totalCounterclockwise > totalClockwise ? totalClockwise + 3 * totalCounterclockwise : totalCounterclockwise + 3 * totalClockwise;
+        long edges = instructions.stream().mapToLong(i -> i.meters() - 1).sum();
+        return area(coordinates()) + (corners + 2 * edges) / 4;
     }
 }
 
 class Puzzle {
-    void solve() throws IOException {
+    void solve(boolean correct) throws IOException {
         try (var input = Objects.requireNonNull(getClass().getResourceAsStream("/day18/day18_input"))) {
             var reader = new BufferedReader(new InputStreamReader(input));
-            var plan = DigPlan.parse(reader.lines());
+            var plan = DigPlan.parse(reader.lines(), correct);
             System.out.println(plan.execute());
         }
     }
