4b962c9f7b3ddcbabe081ad91498e6fdd03047cb
Michiel Boland <michiel@boland.org>
2023-12-25 18:00:57 +0100

    Do day 25 properly

--- src/main/java/day25/Day25.java
+++ src/main/java/day25/Day25.java
@@ -4,12 +4,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 record Apparatus(Map<String, Set<String>> connections) {
@@ -17,6 +21,10 @@ record Apparatus(Map<String, Set<String>> connections) {
         var apparatus = new Apparatus(new HashMap<>());
         lines.forEach(apparatus::parse);
         return apparatus;
+    }
+
+    static String combine(String a, String b) {
+        return a.compareTo(b) < 0 ? a + " " + b : b + " " + a;
     }
 
     void link(String from, String to) {
@@ -46,7 +54,7 @@ record Apparatus(Map<String, Set<String>> connections) {
         Arrays.stream(lr[1].split(" ")).forEach(b -> addConnection(a, b));
     }
 
-    int dijkstra(String node) {
+    Map<String, ND> dijkstra(String node) {
         var queue = new PriorityQueue<ND>();
         Set<String> visited = new HashSet<>();
         Map<String, ND> distanceMap = new HashMap<>();
@@ -62,24 +70,28 @@ record Apparatus(Map<String, Set<String>> connections) {
                         queue.add(new ND(s, current.distance + 1));
                     });
         }
-        return visited.size();
+        return distanceMap;
     }
 
     void solve() {
         String randomNode = connections.keySet().stream().findFirst().orElseThrow();
-        var totalSize = dijkstra(randomNode);
-        CutThese.LINKS.forEach(this::removeConnection);
-        var reducedSize = dijkstra(randomNode);
+        var totalSize = dijkstra(randomNode).size();
+        var allNodes = connections.values().stream().flatMap(Collection::stream).collect(Collectors.toSet());
+        record PathComponent(String label, AtomicInteger counter) implements Comparable<PathComponent> {
+            @Override
+            public int compareTo(PathComponent o) {
+                var c = Integer.compare(counter.get(), o.counter.get());
+                return c != 0 ? c : label.compareTo(o.label);
+            }
+        }
+        Map<String, PathComponent> pathComponents = new HashMap<>();
+        allNodes.forEach(s -> dijkstra(s).forEach((k, v) -> {
+            var label = combine(k, v.node);
+            pathComponents.computeIfAbsent(label, t -> new PathComponent(t, new AtomicInteger())).counter.incrementAndGet();
+        }));
+        pathComponents.values().stream().sorted(Comparator.reverseOrder()).limit(3L).map(PathComponent::label).map(s -> s.split(" ")).forEach(s -> removeConnection(s[0], s[1]));
+        var reducedSize = dijkstra(randomNode).size();
         System.out.println(reducedSize * (totalSize - reducedSize));
-    }
-
-    @Override
-    public String toString() {
-        var sb = new StringBuilder();
-        sb.append("graph day25 {").append(System.lineSeparator());
-        connections.forEach((key, value) -> value.stream().filter(b -> key.compareTo(b) < 0).forEach(b -> sb.append("  ").append(key).append(" -- ").append(b).append(";").append(System.lineSeparator())));
-        sb.append("}");
-        return sb.toString();
     }
 
     record ND(String node, int distance) implements Comparable<ND> {
@@ -97,7 +109,6 @@ class Puzzle {
         try (var input = Objects.requireNonNull(getClass().getResourceAsStream("/day25/day25_input"))) {
             var reader = new BufferedReader(new InputStreamReader(input));
             var apparatus = Apparatus.parse(reader.lines());
-            //System.out.println(apparatus);
             apparatus.solve();
         }
     }
