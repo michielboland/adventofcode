88e12c77f82a9f5f6dee8d9fd0ee0f1a6e95698e
Michiel Boland <michiel@boland.org>
2024-12-03 20:22:33 +0100

    Day 3 part 2

--- src/main/java/year2024/day3/Day3.java
+++ src/main/java/year2024/day3/Day3.java
@@ -7,26 +7,40 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day3 {
     public static void main(String[] args) throws IOException {
-        new Puzzle().solve();
+        new Puzzle().solve(false);
+        new Puzzle().solve(true);
     }
 }
 
 class Puzzle {
-    void solve() throws IOException {
-        var pattern = Pattern.compile("\\((\\d{1,3}),(\\d{1,3})\\).*");
+    void solve(boolean withConditionals) throws IOException {
         try (var input = Objects.requireNonNull(getClass().getResourceAsStream("day3_input"))) {
             var reader = new BufferedReader(new InputStreamReader(input));
-            long sum = reader.lines()
-                    .flatMap(s -> Arrays.stream(s.split("mul")))
-                    .map(pattern::matcher)
-                    .filter(Matcher::matches)
-                    .peek(m -> System.out.println(m.group(0)))
-                    .mapToLong(m -> Long.parseLong(m.group(1)) * Long.parseLong(m.group(2)))
-                    .sum();
-            System.out.println(sum);
+            Stream<String> bits = withConditionals ? wrap(reader.lines()) : reader.lines();
+            System.out.println(sum(bits));
         }
+    }
+
+    long sum(Stream<String> bits) {
+        var pattern = Pattern.compile("\\((\\d{1,3}),(\\d{1,3})\\).*");
+        return bits
+                .flatMap(s -> Arrays.stream(s.split("mul")))
+                .map(pattern::matcher)
+                .filter(Matcher::matches)
+                .mapToLong(m -> Long.parseLong(m.group(1)) * Long.parseLong(m.group(2)))
+                .sum();
+    }
+
+    @SuppressWarnings("DataFlowIssue")
+    Stream<String> wrap(Stream<String> lines) {
+        return Arrays.stream(lines
+                        .collect(Collectors.joining())
+                        .split("do\\(\\)"))
+                .map(s -> s.replaceFirst("don't\\(\\).*", ""));
     }
 }
