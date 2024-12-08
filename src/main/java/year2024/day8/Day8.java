705c298203705718fe2956a93d331e760bd16912
Michiel Boland <michiel@boland.org>
2024-12-08 12:05:05 +0100

    Day 8 part 2

--- src/main/java/year2024/day8/Day8.java
+++ src/main/java/year2024/day8/Day8.java
@@ -4,11 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.stream.Stream;
 
 public class Day8 {
@@ -28,39 +27,50 @@ class Puzzle {
     }
 
     void solve() {
-        System.out.println(grid.antinodes().size());
+        System.out.println(grid.antinodes(false).size());
+        System.out.println(grid.antinodes(true).size());
     }
 }
 
-record Coordinate(int x, int y) implements Comparable<Coordinate> {
+record Coordinate(int x, int y) {
     Coordinate antinode(Coordinate other) {
         return new Coordinate(2 * x() - other.x(), 2 * y() - other.y());
     }
 
-    @Override
-    public int compareTo(Coordinate o) {
-        return y == o.y ? Integer.compare(x, o.x) : Integer.compare(y, o.y);
-    }
-
-    @Override
-    public String toString() {
-        return "(" + x + "," + y + ")";
+    Set<Coordinate> antinodes(Coordinate other, int width, int height, boolean resonant) {
+        Set<Coordinate> antinodes = new HashSet<>();
+        if (resonant) {
+            antinodes.add(this);
+        }
+        var a = this;
+        var b = other;
+        boolean within;
+        do {
+            var c = a.antinode(b);
+            within = c.x() >= 0 && c.x() < width && c.y() >= 0 && c.y() < height;
+            if (within) {
+                antinodes.add(c);
+            }
+            b = a;
+            a = c;
+        } while (within && resonant);
+        return antinodes;
     }
 }
 
-record Grid(Map<Integer, SortedSet<Coordinate>> antennas, Map<Coordinate, Integer> coordinates, int width, int height) {
+record Grid(Map<Integer, Set<Coordinate>> antennas, Map<Coordinate, Integer> coordinates, int width, int height) {
 
     static Grid from(Stream<String> lines) {
         int width = 0;
         int y = 0;
-        Map<Integer, SortedSet<Coordinate>> antennas = new HashMap<>();
+        Map<Integer, Set<Coordinate>> antennas = new HashMap<>();
         Map<Coordinate, Integer> coordinates = new HashMap<>();
         for (String line : lines.toList()) {
             int x = 0;
             for (int c : line.chars().toArray()) {
                 if (c != '.') {
                     var coordinate = new Coordinate(x, y);
-                    antennas.computeIfAbsent(c, k -> new TreeSet<>());
+                    antennas.computeIfAbsent(c, k -> new HashSet<>());
                     antennas.get(c).add(coordinate);
                     coordinates.put(coordinate, c);
                 }
@@ -72,17 +82,14 @@ record Grid(Map<Integer, SortedSet<Coordinate>> antennas, Map<Coordinate, Intege
         return new Grid(antennas, coordinates, width, y);
     }
 
-    Set<Coordinate> antinodes() {
-        Set<Coordinate> antinodes = new TreeSet<>();
-        for (Integer i : antennas.keySet()) {
-            var set = antennas.get(i);
+    Set<Coordinate> antinodes(boolean resonant) {
+        Set<Coordinate> antinodes = new HashSet<>();
+        for (Integer type : antennas.keySet()) {
+            var set = antennas.get(type);
             for (var a : set) {
                 for (var b : set) {
                     if (!a.equals(b)) {
-                        var c = a.antinode(b);
-                        if (c.x() >= 0 && c.x() < width && c.y() >= 0 && c.y() < height) {
-                            antinodes.add(c);
-                        }
+                        antinodes.addAll(a.antinodes(b, width, height, resonant));
                     }
                 }
             }
