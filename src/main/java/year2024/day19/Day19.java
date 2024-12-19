328ff7c48e48adb3bf89bc83bb9c7caa0690362e
Michiel Boland <michiel@boland.org>
2024-12-19 22:45:41 +0100

    Day 19 part 2

--- src/main/java/year2024/day19/Day19.java
+++ src/main/java/year2024/day19/Day19.java
@@ -3,7 +3,9 @@ package year2024.day19;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,6 +20,7 @@ class Puzzle {
     final Set<String> towels;
     final List<String> patterns;
     final int maxLen;
+    Map<String, Long> solutions = new HashMap<>();
 
     Puzzle() throws Exception {
         var input = Files.readString(Paths.get(Objects.requireNonNull(getClass().getResource("day19_input")).toURI()));
@@ -27,38 +30,24 @@ class Puzzle {
         maxLen = towels.stream().mapToInt(String::length).max().orElseThrow();
     }
 
-    long solutions(String s) {
-        return solutions(s, 0, s.length());
-    }
-
-    long solutions(String pattern, int from, int to) {
-        var portion = pattern.substring(from, to);
-        if (portion.isEmpty()) {
+    long solutions(String pattern) {
+        if (pattern.isEmpty()) {
             return 1;
         }
-        if (towels.contains(portion)) {
-            return 1;
+        if (solutions.containsKey(pattern)) {
+            return solutions.get(pattern);
         }
-        if (to == from + 1) {
-            return 0;
-        }
-        int middle = (from + to) / 2;
-        long total = solutions(pattern, from, middle) * solutions(pattern, middle, to);
-        for (int towelSize = 2; towelSize <= maxLen; towelSize++) {
-            for (int offset = middle - towelSize + 1; offset < middle; offset++) {
-                if (offset < from || offset + towelSize > to) {
-                    continue;
-                }
-                if (towels.contains(pattern.substring(offset, offset + towelSize))) {
-                    total += solutions(pattern, from, offset) * solutions(pattern, offset + towelSize, to);
-                }
+        long total = 0;
+        for (int i = 1; i <= maxLen && i <= pattern.length(); i++) {
+            if (towels.contains(pattern.substring(0, i))) {
+                total += solutions(pattern.substring(i));
             }
         }
+        solutions.put(pattern, total);
         return total;
     }
 
     void solve() {
-        // 76514031378 is too low
         System.out.println(patterns.stream().filter(p -> solutions(p) > 0).count());
         System.out.println(patterns.stream().mapToLong(this::solutions).sum());
     }
