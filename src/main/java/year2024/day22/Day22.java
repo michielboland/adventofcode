16c60603b6147f15b324d715af86ef305ed32fd4
Michiel Boland <michiel@boland.org>
2024-12-22 16:59:27 +0100

    Day 22 part 2 without brute force

--- src/main/java/year2024/day22/Day22.java
+++ src/main/java/year2024/day22/Day22.java
@@ -2,8 +2,16 @@ package year2024.day22;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day22 {
@@ -22,118 +30,63 @@ class Puzzle {
     }
 
     void solve() {
-        market.repeatIterateAllBuyers();
-        System.out.println(market.sumSecrets());
-        market.bruteForceIt();
+        System.out.println(market.iterateAllBuyers());
     }
+}
+
+record Pair(long sumSecrets, int bestPrice) {
 }
 
 class Market {
     static final int ITERATIONS = 2000;
+    static final int SEQUENCE_LENGTH = 4;
     static final int B24 = 0xffffff;
-    private final int[] secrets;
-    private final byte[][] prices;
-    private final byte[][] deltas;
+    private final List<Integer> secrets;
 
     Market(Stream<String> secrets) {
-        this.secrets = secrets.mapToInt(Integer::parseInt).toArray();
-        prices = new byte[this.secrets.length][];
-        deltas = new byte[this.secrets.length][];
-        for (int i = 0; i < prices.length; i++) {
-            prices[i] = new byte[ITERATIONS];
-        }
-        for (int i = 0; i < deltas.length; i++) {
-            deltas[i] = new byte[ITERATIONS];
-        }
+        this.secrets = secrets.map(Integer::parseInt).toList();
     }
 
-    static int findFirst(byte[] haystack, byte[] needle) {
-        for (int i = 0; i <= haystack.length - needle.length; i++) {
-            boolean found = true;
-            for (int j = 0; found && j < needle.length; j++) {
-                found = needle[j] == haystack[i + j];
+    void iterateBuyer(final int initialSecret, BiConsumer<String, Integer> priceWatcher, Consumer<Integer> secretWatcher) {
+        int secret = initialSecret;
+        List<Integer> deltas = new ArrayList<>();
+        Set<String> visited = new HashSet<>();
+        for (int round = 0; round < ITERATIONS; round++) {
+            int oldPrice = secret % 10;
+            secret = (secret << 6 ^ secret) & B24;
+            secret = (secret >> 5 ^ secret) & B24;
+            secret = (secret << 11 ^ secret) & B24;
+            int newPrice = secret % 10;
+            if (deltas.size() >= SEQUENCE_LENGTH) {
+                deltas.remove(0);
             }
-            if (found) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    int bananas(byte[] pattern) {
-        if (impossible(pattern)) {
-            return 0;
-        }
-        int bananas = 0;
-        for (int i = 0; i < deltas.length; i++) {
-            int pos = findFirst(deltas[i], pattern);
-            if (pos != -1) {
-                bananas += prices[i][pos + 3];
-            }
-        }
-        return bananas;
-    }
-
-    boolean impossible(byte[] pattern) {
-        boolean possible;
-        for (byte start = -9; start <= 9; start++) {
-            possible = true;
-            int t = start;
-            for (byte b : pattern) {
-                t = t + b;
-                if (t < -9 || t > 9) {
-                    possible = false;
-                    break;
-                }
-            }
-            if (possible) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    void bruteForceIt() {
-        int bestPrice = 0;
-        byte[] tryThese = new byte[]{0, 1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6, 7, -7, 8, -8, 9, -9};
-        for (byte i : tryThese) {
-            for (byte j : tryThese) {
-                for (byte k : tryThese) {
-                    for (byte l : tryThese) {
-                        byte[] pattern = {i, j, k, l};
-                        var price = bananas(pattern);
-                        if (price > bestPrice) {
-                            bestPrice = price;
-                            System.out.printf("pattern (%d,%d,%d,%d) -> price %d%n", i, j, k, l, price);
-                        }
-                    }
+            deltas.add(newPrice - oldPrice);
+            if (deltas.size() >= SEQUENCE_LENGTH) {
+                var pattern = deltas.stream().map(String::valueOf).collect(Collectors.joining());
+                if (!visited.contains(pattern)) {
+                    visited.add(pattern);
+                    priceWatcher.accept(pattern, newPrice);
                 }
             }
         }
-        System.out.println("best price = " + bestPrice);
+        secretWatcher.accept(secret);
     }
 
-    void iterateBuyer(int round, int buyer) {
-        int secret = secrets[buyer];
-        int oldPrice = secret % 10;
-        secret = (secret << 6 ^ secret) & B24;
-        secret = (secret >> 5 ^ secret) & B24;
-        secret = (secret << 11 ^ secret) & B24;
-        int newPrice = secret % 10;
-        secrets[buyer] = secret;
-        prices[buyer][round] = (byte) newPrice;
-        deltas[buyer][round] = (byte) (newPrice - oldPrice);
-    }
+    Pair iterateAllBuyers() {
+        Map<String, Integer> prices = new HashMap<>();
+        class SumHolder {
+            long sum;
 
-    void iterateAllBuyers(int i) {
-        for (int j = 0; j < secrets.length; j++) iterateBuyer(i, j);
-    }
-
-    void repeatIterateAllBuyers() {
-        for (int i = 0; i < ITERATIONS; i++) iterateAllBuyers(i);
-    }
-
-    long sumSecrets() {
-        return Arrays.stream(secrets).mapToLong(i -> i).sum();
+            void add(int i) {
+                sum += i;
+            }
+        }
+        var sumHolder = new SumHolder();
+        secrets.forEach(initialSecret -> iterateBuyer(
+                initialSecret,
+                ((pattern, price) -> prices.compute(pattern, (k, v) -> v == null ? price : v + price)),
+                sumHolder::add
+        ));
+        return new Pair(sumHolder.sum, prices.values().stream().max(Integer::compareTo).orElseThrow());
     }
 }
