d7277b5636170b3302e062097371ad1485cf9322
Michiel Boland <michiel@boland.org>
2024-12-25 00:59:22 +0100

    Day 24 part 2

--- src/main/java/year2024/day24/Day24.java
+++ src/main/java/year2024/day24/Day24.java
@@ -2,12 +2,11 @@ package year2024.day24;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 enum GateType {
@@ -38,18 +37,25 @@ public class Day24 {
 }
 
 class Puzzle {
-    final Map<String, Integer> initialValues = new TreeMap<>();
-    final Map<String, Wire> wires = new TreeMap<>();
+    final Map<String, Integer> initialValues = new HashMap<>();
+    final Map<String, Wire> wires = new HashMap<>();
 
     Puzzle() throws Exception {
         var parts = Files.readString(Paths.get(Objects.requireNonNull(getClass().getResource("day24_input")).toURI())).split("\n\n");
-        for (String initLine : parts[0].split("\n")) {
+        part0(parts[0]);
+        part1(parts[1]);
+    }
+
+    private void part0(String lines) {
+        for (String initLine : lines.split("\n")) {
             var initSplit = initLine.split(": ");
             String name = initSplit[0];
-            wires.computeIfAbsent(name, k -> new Wire(name));
             initialValues.put(name, Integer.parseInt(initSplit[1]));
         }
-        for (String gateLine : parts[1].split("\n")) {
+    }
+
+    private void part1(String lines) {
+        for (String gateLine : lines.split("\n")) {
             var split = gateLine.split(" ");
             var nameA = split[0];
             var type = GateType.valueOf(split[1]);
@@ -64,55 +70,91 @@ class Puzzle {
         }
     }
 
+    long getValue() {
+        long value = 0L;
+        long bit = 1L;
+        int i = 0;
+        do {
+            var wire = wires.get(("z" + "%02d").formatted(i));
+            if (wire == null) {
+                break;
+            }
+            if (wire.getValue() == 1) {
+                value |= bit;
+            }
+            ++i;
+            bit <<= 1;
+        } while (true);
+        return value;
+    }
+
+    void traceAll() {
+        Set<String> swaps = new HashSet<>();
+        int i = 0;
+        do {
+            ++i;
+        } while (cell(i, swaps));
+        System.out.println(swaps.stream().sorted().collect(Collectors.joining(",")));
+    }
+
+    boolean cell(int i, Set<String> swaps) {
+        var a = wires.get(("x" + "%02d").formatted(i));
+        if (a == null) {
+            return false;
+        }
+        var xorGate = a.gates().stream().filter(g -> g.type() == GateType.XOR).findFirst().orElseThrow();
+        var xorOutput = xorGate.output();
+        var andGate = a.gates().stream().filter(g -> g.type() == GateType.AND).findFirst().orElseThrow();
+        var andOutput = andGate.output();
+        if (xorOutput.gates().stream().noneMatch(g -> g.type() == GateType.XOR) && andOutput.gates().stream().anyMatch(g -> g.type() == GateType.XOR)) {
+            swaps.add(xorOutput.name());
+            swaps.add(andOutput.name());
+        }
+        var outputName = xorOutput.gates().stream().filter(g -> g.type() == GateType.XOR).findFirst().map(g -> g.output().name()).orElse(null);
+        if (outputName != null && !outputName.equals("z%02d".formatted(i))) {
+            swaps.add(outputName);
+            swaps.add("z%02d".formatted(i));
+        }
+        return true;
+    }
+
     void solve() {
-        initialValues.forEach((k, v) -> wires.get(k).value(v));
-        var outputNames = wires.keySet().stream().filter(s -> s.startsWith("z")).sorted(Comparator.reverseOrder()).toList();
-        String binary = outputNames.stream().map(s -> String.valueOf(wires.get(s).value())).collect(Collectors.joining());
-        System.out.println(binary);
-        System.out.println(Long.parseLong(binary, 2));
+        initialValues.forEach((k, v) -> wires.get(k).setValue(v));
+        System.out.println(getValue());
+        traceAll();
     }
 }
 
-class Holder {
+class State {
     Integer value;
 
-    boolean setValue(Integer value) {
-        if (this.value == null) {
-            this.value = Objects.requireNonNull(value);
-            return true;
-        }
-        if (!this.value.equals(value)) {
+    void setValue(int value) {
+        if (this.value != null) {
             throw new IllegalStateException();
         }
-        return false;
+        this.value = value;
     }
 }
 
-record Wire(String name, Holder state, Set<Gate> gates) implements Comparable<Wire> {
+record Wire(String name, State state, Set<Gate> gates) {
     Wire(String name) {
-        this(name, new Holder(), new HashSet<>());
+        this(name, new State(), new HashSet<>());
     }
 
-    Integer value() {
+    Integer getValue() {
         return state.value;
     }
 
-    void value(Integer value) {
-        if (state.setValue(value)) {
-            for (var gate : gates) {
-                gate.trigger();
-            }
+    void setValue(int value) {
+        state.setValue(value);
+        for (var gate : gates) {
+            gate.trigger();
         }
     }
 
     @Override
     public String toString() {
-        return name + "(" + state.value + ")";
-    }
-
-    @Override
-    public int compareTo(Wire o) {
-        return name.compareTo(o.name);
+        return name;
     }
 
     @Override
@@ -120,19 +162,19 @@ record Wire(String name, Holder state, Set<Gate> gates) implements Comparable<Wi
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Wire wire = (Wire) o;
-        return Objects.equals(name, wire.name) && Objects.equals(state, wire.state);
+        return Objects.equals(name, wire.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, state);
+        return Objects.hashCode(name);
     }
 }
 
 record Gate(GateType type, Wire inputA, Wire inputB, Wire output) {
     void trigger() {
-        if (inputA.value() != null && inputB.value() != null) {
-            output.value(type.apply(inputA.value(), inputB.value()));
+        if (inputA.getValue() != null && inputB.getValue() != null) {
+            output.setValue(type.apply(inputA.getValue(), inputB.getValue()));
         }
     }
 }
