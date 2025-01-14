799ae44130fe442b20ae043edd152fd2ef13f9e9
Michiel Boland <michiel@boland.org>
2025-01-14 22:23:12 +0100

    Year 2022 day 11 part 2

--- src/main/java/year2022/day11/Puzzle.java
+++ src/main/java/year2022/day11/Puzzle.java
@@ -1,29 +1,26 @@
 package year2022.day11;
 
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.SortedMap;
 import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
 public class Puzzle {
-    final SortedMap<Integer, Monkey> monkeys = new TreeMap<>();
+    final List<String> blocks;
 
     Puzzle() throws Exception {
         var input = Files.readString(Paths.get(Objects.requireNonNull(getClass().getResource("day11_input")).toURI()));
-        for (String lines : input.split("\n\n")) {
-            var monkey = Monkey.from(lines, monkeys);
-            if (monkeys.containsKey(monkey.id())) {
-                throw new IllegalStateException();
-            }
-            monkeys.put(monkey.id(), monkey);
-        }
+        blocks = Arrays.stream(input.split("\n\n")).toList();
     }
 
     public static void main(String[] args) throws Exception {
@@ -31,17 +28,34 @@ public class Puzzle {
     }
 
     void solve() {
-        for (int i = 0; i < 20; i++) {
-            round();
+        System.out.println(Monkeys.from(blocks, false).business());
+        System.out.println(Monkeys.from(blocks, true).business());
+    }
+}
+
+record Monkeys(SortedMap<Integer, Monkey> monkeys, boolean worried, long mod) {
+    static Monkeys from(List<String> blocks, boolean worried) {
+        SortedMap<Integer, Monkey> monkeys = new TreeMap<>();
+        for (var lines : blocks) {
+            var monkey = Monkey.from(lines, monkeys);
+            monkeys.put(monkey.id(), monkey);
         }
-        var top2 = monkeys.keySet().stream().map(i -> monkeys.get(i).inspectedItems().size()).sorted(Collections.reverseOrder()).limit(2).toList();
-        System.out.println(top2.get(0) * top2.get(1));
+        BigInteger b = monkeys.values().stream().map(m -> BigInteger.valueOf(m.divisor())).reduce(BigInteger.ONE, BigInteger::multiply);
+        if (b.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0) {
+            throw new IllegalArgumentException();
+        }
+        return new Monkeys(monkeys, worried, b.longValue());
     }
 
-    void round() {
-        for (Integer i : monkeys.keySet()) {
-            monkeys.get(i).passItems();
+    long business() {
+        var rounds = worried ? 10000 : 20;
+        for (var round = 0; round < rounds; round++) {
+            for (Integer i : monkeys.keySet()) {
+                monkeys.get(i).passItems(worried, mod);
+            }
         }
+        var top2 = monkeys.keySet().stream().map(i -> monkeys.get(i).inspectedItems().get()).sorted(Collections.reverseOrder()).limit(2).toList();
+        return (long) top2.get(0) * top2.get(1);
     }
 }
 
@@ -53,7 +67,7 @@ record Monkey(
         int trueMonkey,
         int falseMonkey,
         Map<Integer, Monkey> monkeys,
-        List<Long> inspectedItems
+        AtomicInteger inspectedItems
 ) {
     static final Pattern OP_PATTERN = Pattern.compile(" {2}Operation: new = old (.) (.*)");
 
@@ -74,13 +88,19 @@ record Monkey(
         }
     }
 
-    void passItems() {
+    void passItems(boolean worried, long mod) {
         var it = items.iterator();
         while (it.hasNext()) {
             long item = it.next();
+            if (worried) {
+                item = item % mod;
+            }
             it.remove();
-            inspectedItems.add(item);
-            long newLevel = operation.apply(item) / 3;
+            inspectedItems.incrementAndGet();
+            long newLevel = operation.apply(item);
+            if (!worried) {
+                newLevel /= 3;
+            }
             int destination = newLevel % divisor == 0 ? trueMonkey : falseMonkey;
             monkeys.get(destination).items.add(newLevel);
         }
@@ -122,7 +142,7 @@ record Monkey(
                 Objects.requireNonNull(trueMonkey),
                 Objects.requireNonNull(falseMonkey),
                 monkeys,
-                new ArrayList<>()
+                new AtomicInteger()
         );
     }
 }
