868066058afb8faed777d50ac75b63a7c2846eed
Michiel Boland <michiel@boland.org>
2025-02-05 23:42:04 +0100

    Year 2022 day 16 part 1 (optimized)

--- src/main/java/year2022/day16/Puzzle.java
+++ src/main/java/year2022/day16/Puzzle.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -16,9 +17,12 @@ import java.util.stream.Collectors;
 
 public class Puzzle {
     private static final Pattern PATTERN = Pattern.compile("Valve (.+) has flow rate=(\\d+); tunnels? leads? to valves? (.+)");
+    public static final String INITIAL_VALVE = "AA";
 
     final Map<String, Integer> valveMap = new HashMap<>();
     final Map<String, Collection<String>> neighbourMap = new HashMap<>();
+    final Map<String, Map<String, Integer>> valveDistances = new HashMap<>();
+    final Map<String, Long> valveBits = new HashMap<>();
 
     Puzzle() throws Exception {
         try (var reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("day16_input"))))) {
@@ -32,11 +36,15 @@ public class Puzzle {
                 neighbourMap.put(name, Arrays.stream(matcher.group(3).split(", ")).collect(Collectors.toSet()));
             }
         }
-        neighbourMap.forEach((k, v) -> v.forEach(l -> {
-            if (!neighbourMap.get(l).contains(k)) {
-                throw new IllegalStateException();
+        valveDistances.put(INITIAL_VALVE, distanceMap(INITIAL_VALVE));
+        final var bit = new Bit();
+        valveMap.forEach((valve, flowRate) -> {
+            if (flowRate >= 0) {
+                valveDistances.put(valve, distanceMap(valve));
+                valveBits.put(valve, bit.value);
+                bit.shift();
             }
-        }));
+        });
     }
 
     public static void main(String[] args) throws Exception {
@@ -47,62 +55,86 @@ public class Puzzle {
         System.out.println(max());
     }
 
+    Map<String, Integer> distanceMap(String origin) {
+        Map<String, Integer> distanceMap = new HashMap<>();
+        var queue = new PriorityQueue<ValveDistance>();
+        Set<String> visited = new HashSet<>();
+        queue.add(new ValveDistance(origin, 0));
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+            if (valveMap.get(current.valve()) > 0) {
+                // Add one for turning on the valve
+                distanceMap.put(current.valve(), current.distance() + 1);
+            }
+            for (String neighbour : neighbourMap.get(current.valve())) {
+                if (!visited.contains(neighbour)) {
+                    visited.add(neighbour);
+                    queue.add(new ValveDistance(neighbour, current.distance() + 1));
+                }
+            }
+        }
+        return distanceMap;
+    }
+
     int max() {
         int max = 0;
         int minutes = 30;
         Queue<ND> queue = new ArrayDeque<>();
-        Set<State> visited = new HashSet<>();
         queue.add(ND.INITIAL);
         while (!queue.isEmpty()) {
             final var current = queue.remove();
-            if (current.minute() == minutes) {
-                int totalPressure = current.totalPressure();
-                if (totalPressure > max) {
-                    max = totalPressure;
-                }
-                continue;
+            int totalPressure = current.finalTotalPressure(minutes);
+            if (totalPressure > max) {
+                max = totalPressure;
             }
             final var valve = current.state().valve();
-            final var openValves = current.state().openValves();
-            boolean canMove = false;
-            for (String neighbour : neighbourMap.get(valve)) {
-                State newState = new State(neighbour, openValves);
-                if (!visited.contains(newState)) {
-                    canMove = true;
-                    visited.add(newState);
-                    queue.add(current.extend(newState, 0));
-                }
-            }
+            final var openValveBitmap = current.state().openValveBitmap();
             final var flowRate = valveMap.get(valve);
-            if (flowRate > 0 && !openValves.contains(valve)) {
-                final var newOpenValves = openValves + valve;
-                State newState = new State(valve, newOpenValves);
-                if (!visited.contains(newState)) {
-                    canMove = true;
-                    visited.add(newState);
-                    queue.add(current.extend(newState, flowRate));
+            valveDistances.get(valve).forEach((newValve, distance) -> {
+                if (current.minute() + distance < minutes) {
+                    var bit = valveBits.get(newValve);
+                    if ((bit & openValveBitmap) == 0) {
+                        queue.add(current.extend(current.state().extend(newValve, bit), flowRate, distance, valveMap.get(newValve)));
+                    }
                 }
-            }
-            if (!canMove) {
-                int totalPressure = current.totalPressure() + current.pressure() * (minutes - current.minute());
-                if (totalPressure > max) {
-                    max = totalPressure;
-                }
-            }
+            });
         }
         return max;
     }
-
 }
 
-record State(String valve, String openValves) {
-    static final State INITIAL = new State("AA", "");
+class Bit {
+    long value = 1L;
+
+    void shift() {
+        value <<= 1;
+    }
 }
 
-record ND(State state, int minute, int pressure, int totalPressure) {
-    static final ND INITIAL = new ND(State.INITIAL, 1, 0, 0);
+record ValveDistance(String valve, int distance) implements Comparable<ValveDistance> {
 
-    ND extend(State state, int flowRate) {
-        return new ND(state, minute + 1, pressure + flowRate, totalPressure + pressure + flowRate);
+    @Override
+    public int compareTo(ValveDistance o) {
+        return distance == o.distance ? valve.compareTo(o.valve) : Integer.compare(distance, o.distance);
+    }
+}
+
+record State(String valve, long openValveBitmap) {
+    static final State INITIAL = new State(Puzzle.INITIAL_VALVE, 0L);
+
+    State extend(String valve, long bit) {
+        return new State(valve, openValveBitmap | bit);
+    }
+}
+
+record ND(State state, int minute, int pressure, int totalPressure, int finalPressure) {
+    static final ND INITIAL = new ND(State.INITIAL, 0, 0, 0, 0);
+
+    ND extend(State state, int flowRate, int distance, int newFlowRate) {
+        return new ND(state, minute + distance, pressure + flowRate, totalPressure + (pressure + flowRate) * distance, pressure + flowRate + newFlowRate);
+    }
+
+    int finalTotalPressure(int minutes) {
+        return totalPressure + finalPressure * (minutes - minute);
     }
 }
