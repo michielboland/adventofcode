f4d1f2a670633894b41009f996908729f61238d6
Michiel Boland <michiel@boland.org>
2025-12-02 19:16:26 +0100

    Year 2025 day 1 part 2

--- src/main/java/year2025/day1/Puzzle.java
+++ src/main/java/year2025/day1/Puzzle.java
@@ -5,45 +5,66 @@ import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class Puzzle {
+    private final Dial dial = new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("day1_input"))))
+            .lines()
+            .map(Instruction::parse)
+            .reduce(Dial.INITIAL, Dial::operate, Dial::combine);
+
     public static void main(String[] args) {
-        var reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(Puzzle.class.getResourceAsStream("day1_input"))));
-        Dial d = reader.lines().map(Instruction::parse).reduce(Dial.INITIAL, Dial::operate, Dial::combine);
-        System.out.println(d.zeros);
+        var puzzle = new Puzzle();
+        System.out.println(puzzle.part1());
+        System.out.println(puzzle.part2());
     }
 
-    static int posMod(int i, @SuppressWarnings("SameParameterValue") int j) {
-        var r = i % j;
-        return r >= 0 ? r : r + j;
+    int part1() {
+        return dial.zeros();
     }
 
-    enum LR {L, R}
+    int part2() {
+        return dial.totalZeros();
+    }
 
-    record Instruction(LR lr, int distance) {
+    record Instruction(int distance) {
         static Instruction parse(String line) {
-            return new Instruction(LR.valueOf(line.substring(0, 1)), Integer.parseInt(line.substring(1)));
+            enum LR {L, R}
+            return switch (LR.valueOf(line.substring(0, 1))) {
+                case L -> new Instruction(-Integer.parseInt(line.substring(1)));
+                case R -> new Instruction(Integer.parseInt(line.substring(1)));
+            };
         }
     }
 
-    record Dial(int number, int zeros) {
-        static final Dial INITIAL = new Dial(50, 0);
+    record Dial(int number, int zeros, int zeroCrossings) {
+        static final Dial INITIAL = new Dial(50, 0, 0);
 
-        Dial click(int newNumber) {
-            return new Dial(newNumber, zeros + (newNumber == 0 ? 1 : 0));
+        int totalZeros() {
+            return zeros + zeroCrossings;
         }
 
-        Dial right(int times) {
-            return click(posMod(number + times, 100));
-        }
-
-        Dial left(int times) {
-            return click(posMod(number - times, 100));
+        Dial update(int newNumber) {
+            int newCrossings = 0;
+            if (newNumber > number) {
+                for (int c = number + 1; c < newNumber; c++) {
+                    if (c % 100 == 0) {
+                        // ugh
+                        ++newCrossings;
+                    }
+                }
+            } else if (number > newNumber) {
+                for (int c = number - 1; c > newNumber; c--) {
+                    if (c % 100 == 0) {
+                        ++newCrossings;
+                    }
+                }
+            } else {
+                throw new IllegalArgumentException();
+            }
+            var mod = newNumber % 100;
+            return new Dial(mod, zeros + (mod == 0 ? 1 : 0), zeroCrossings + newCrossings);
         }
 
         Dial operate(Instruction instruction) {
-            return switch (instruction.lr) {
-                case L -> left(instruction.distance);
-                case R -> right(instruction.distance);
-            };
+            return update(number + instruction.distance);
         }
 
         public Dial combine(Dial other) {
