b4f27a585f15017985b980a39b8d47b6a4e98a8a
Michiel Boland <michiel@boland.org>
2025-12-03 09:53:43 +0100

    Year 2025 day 2 part 1

--- src/main/java/year2025/day2/Puzzle.java
+++ src/main/java/year2025/day2/Puzzle.java
@@ -2,11 +2,11 @@ package year2025.day2;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 public class Puzzle {
 
@@ -24,50 +24,25 @@ public class Puzzle {
     }
 
     private long part1() {
-        // 28845472378 is too low
         return ranges.stream().flatMap(r -> r.invalidIds().stream()).mapToLong(l -> l).sum();
     }
 }
 
 record Range(long min, long max) {
+    private static final Pattern REPEAT_TWO = Pattern.compile("(.+)\\1");
+
     static Range parse(String s) {
         var lr = s.split("-");
         return new Range(Long.parseLong(lr[0]), Long.parseLong(lr[1]));
     }
 
     List<Long> invalidIds() {
-        var left = String.valueOf(min);
-        var right = String.valueOf(max);
-        String startBits;
-        if (right.length() != left.length()) {
-            if (right.length() != left.length() + 1) {
-                throw new IllegalStateException();
-            }
-            if (left.length() % 2 == 0) {
-                startBits = left.substring(0, left.length() / 2);
-            } else {
-                startBits = right.substring(0, right.length() / 2);
-            }
-        } else {
-            if (left.length() % 2 != 0) {
-                return List.of();
-            } else {
-                startBits = left.substring(0, left.length() / 2);
+        List<Long> invalidIds = new ArrayList<>();
+        for (var i = min; i <= max; i++) {
+            if (REPEAT_TWO.matcher(String.valueOf(i)).matches()) {
+                invalidIds.add(i);
             }
         }
-        var l = Long.parseLong(startBits);
-        Set<Long> invalidIds = new TreeSet<>();
-        do {
-            var s = String.valueOf(l).repeat(2);
-            var candidate = Long.parseLong(s);
-            if (candidate > max) {
-                break;
-            }
-            if (candidate >= min) {
-                invalidIds.add(candidate);
-            }
-            ++l;
-        } while (true);
         return List.copyOf(invalidIds);
     }
 }
