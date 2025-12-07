ab7c666838f5c692a369ddfee633134c77a5d029
Michiel Boland <michiel@boland.org>
2025-12-07 13:23:22 +0100

    Simplify

--- src/main/java/year2025/day7/Puzzle.java
+++ src/main/java/year2025/day7/Puzzle.java
@@ -2,20 +2,17 @@ package year2025.day7;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.SortedMap;
-import java.util.SortedSet;
-import java.util.TreeMap;
+import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Puzzle {
 
-    private final Grid initialGrid = Grid.parse(new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("day7_input")))).lines());
+    private final Grid grid = Grid.parse(new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("day7_input")))).lines());
 
     public static void main(String[] args) {
         new Puzzle().solve();
@@ -26,16 +23,12 @@ public class Puzzle {
     }
 
     int part1() {
-        var grid = initialGrid;
-        do {
-            grid = grid.iterate();
-        } while (!grid.done());
         return grid.splits();
     }
 }
 
 enum Symbol {
-    FREE('.'), SPLITTER('^'), START('S'), BEAM('|');
+    FREE('.'), SPLITTER('^'), START('S');
 
     final char value;
 
@@ -48,12 +41,11 @@ enum Symbol {
     }
 }
 
-record Grid(SortedMap<Coordinate, Symbol> map, SortedSet<Coordinate> beamPositions, int splits, int width, int height) {
+record Grid(Set<Coordinate> splitters, Coordinate start, int height) {
     static Grid parse(Stream<String> lines) {
-        var map = new TreeMap<Coordinate, Symbol>();
-        var beamPositions = new TreeSet<Coordinate>();
+        var starts = new TreeSet<Coordinate>();
+        var splitters = new TreeSet<Coordinate>();
         var yc = new AtomicInteger();
-        var widths = new TreeSet<Integer>();
         lines.forEach(l -> {
             var y = yc.getAndIncrement();
             var xc = new AtomicInteger();
@@ -61,59 +53,42 @@ record Grid(SortedMap<Coordinate, Symbol> map, SortedSet<Coordinate> beamPositio
                 var x = xc.getAndIncrement();
                 var coordinate = new Coordinate(x, y);
                 var symbol = Symbol.of((char) c);
-                if (symbol == Symbol.START) {
-                    beamPositions.add(coordinate);
+                switch (symbol) {
+                    case SPLITTER -> splitters.add(coordinate);
+                    case START -> starts.add(coordinate);
                 }
-                map.put(coordinate, symbol);
             });
-            widths.add(xc.get());
         });
-        return new Grid(map, beamPositions, 0, widths.getFirst(), yc.get());
+        return new Grid(splitters, starts.first(), yc.get());
     }
 
-    boolean done() {
-        return beamPositions.isEmpty();
-    }
-
-    Grid iterate() {
-        var newMap = new TreeMap<>(map);
-        var newBeamPositions = new TreeSet<Coordinate>();
-        // pass 1 - beam passes straight through
-        for (Coordinate beamPosition : beamPositions) {
-            var south = beamPosition.south();
-            var symbol = newMap.get(south);
-            if (symbol == Symbol.FREE) {
-                newMap.put(south, Symbol.BEAM);
-                newBeamPositions.add(south);
-            }
-        }
-        int newSplits = 0;
-        // pass 2 - possible split
-        for (Coordinate beamPosition : beamPositions) {
-            var south = beamPosition.south();
-            var symbol = newMap.get(south);
-            if (symbol == Symbol.SPLITTER) {
-                newSplits++;
-                Stream.of(south.west(), south.east()).forEach(candidate -> {
-                    if (newMap.get(candidate) == Symbol.FREE) {
-                        newMap.put(candidate, Symbol.BEAM);
-                        newBeamPositions.add(candidate);
+    int splits() {
+        var deque = new ArrayDeque<Coordinate>();
+        var visited = new TreeSet<Coordinate>();
+        deque.addLast(start);
+        visited.add(start);
+        int splits = 0;
+        while (!deque.isEmpty()) {
+            var c = deque.removeFirst();
+            var south = c.south();
+            if (south.y() < height) {
+                if (splitters.contains(south)) {
+                    splits++;
+                    Stream.of(south.west(), south.east()).forEach(candidate -> {
+                        if (!visited.contains(candidate)) {
+                            visited.add(candidate);
+                            deque.addLast(candidate);
+                        }
+                    });
+                } else {
+                    if (!visited.contains(south)) {
+                        visited.add(south);
+                        deque.addLast(south);
                     }
-                });
+                }
             }
         }
-        return new Grid(newMap, newBeamPositions, splits + newSplits, width, height);
-    }
-
-    @Override
-    public String toString() {
-        return IntStream.range(0, height).mapToObj(y -> {
-            var s = new char[width];
-            for (int x = 0; x < width; x++) {
-                s[x] = map.get(new Coordinate(x, y)).value;
-            }
-            return new String(s);
-        }).collect(Collectors.joining("\n"));
+        return splits;
     }
 }
 
@@ -122,10 +97,6 @@ record Coordinate(int p) implements Comparable<Coordinate> {
 
     public Coordinate(int x, int y) {
         this(x + M * y);
-    }
-
-    Coordinate north() {
-        return new Coordinate(p - M);
     }
 
     Coordinate south() {
@@ -138,6 +109,10 @@ record Coordinate(int p) implements Comparable<Coordinate> {
 
     Coordinate west() {
         return new Coordinate(p - 1);
+    }
+
+    int y() {
+        return p / M;
     }
 
     @Override
