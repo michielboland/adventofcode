998360558fba6df1494ef16d4696bd5fbb3bb3f0
Michiel Boland <michiel@boland.org>
2025-12-08 15:39:12 +0100

    Year 2025 day 8 part 2

--- src/main/java/year2025/day8/Puzzle.java
+++ src/main/java/year2025/day8/Puzzle.java
@@ -12,7 +12,21 @@ import java.util.Set;
 public class Puzzle {
 
     static final int CONNECTIONS = 1000;
-    private final List<Box> boxes = new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("day8_input")))).lines().map(Box::parse).toList();
+    private final int boxCount;
+    private final List<Pair> sorted;
+    private final List<Set<Box>> circuits = new ArrayList<>();
+
+    Puzzle() {
+        var pairs = new ArrayList<Pair>();
+        var boxes = new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("day8_input")))).lines().map(Box::parse).toList();
+        boxCount = boxes.size();
+        for (int i = 0; i < boxes.size(); i++) {
+            for (int j = i + 1; j < boxes.size(); j++) {
+                pairs.add(Pair.of(boxes.get(i), boxes.get(j)));
+            }
+        }
+        sorted = pairs.stream().sorted(Comparator.comparing(Pair::distance)).toList();
+    }
 
     public static void main(String[] args) {
         new Puzzle().solve();
@@ -20,57 +34,63 @@ public class Puzzle {
 
     private void solve() {
         System.out.println(part1());
+        System.out.println(part2());
     }
 
     private long part1() {
-        List<Pair> pairs = new ArrayList<>();
-        for (int i = 0; i < boxes.size(); i++) {
-            for (int j = i + 1; j < boxes.size(); j++) {
-                pairs.add(Pair.of(boxes.get(i), boxes.get(j)));
-            }
-        }
-        var sorted = pairs.stream().sorted(Comparator.comparing(Pair::distance)).toList();
-        List<Set<Box>> circuits = new ArrayList<>();
         for (int i = 0; i < CONNECTIONS && i < sorted.size(); i++) {
-            var pair = sorted.get(i);
-            boolean found = false;
-            for (int j = 0; j < circuits.size(); j++) {
-                var left = circuits.get(j);
-                var aInLeft = left.contains(pair.a());
-                var bInLeft = left.contains(pair.b());
-                if (aInLeft && bInLeft) {
-                    found = true;
-                    break;
-                }
-                if (aInLeft || bInLeft) {
-                    for (int k = j + 1; k < circuits.size(); k++) {
-                        var right = circuits.get(k);
-                        var aInRight = right.contains(pair.a());
-                        var bInRight = right.contains(pair.b());
-                        if (aInRight || bInRight) {
-                            found = true;
-                            left.addAll(right);
-                            circuits.remove(k);
-                            break;
-                        }
-                    }
-                    if (!found) {
-                        left.add(aInLeft ? pair.b() : pair.a());
-                        found = true;
-                    }
-                    break;
-                }
-            }
-            if (!found) {
-                var newCircuit = new HashSet<Box>();
-                newCircuit.add(pair.a());
-                newCircuit.add(pair.b());
-                circuits.add(newCircuit);
-            }
+            connect(sorted.get(i));
         }
         var sortedSizes = circuits.stream().mapToLong(Set::size).sorted().toArray();
         var l = sortedSizes.length;
         return sortedSizes[l - 1] * sortedSizes[l - 2] * sortedSizes[l - 3];
+    }
+
+    private long part2() {
+        for (int i = CONNECTIONS; ; i++) {
+            var pair = sorted.get(i);
+            connect(pair);
+            if (circuits.size() == 1 && circuits.getFirst().size() == boxCount) {
+                return pair.a().x() * pair.b().x();
+            }
+        }
+    }
+
+    private void connect(Pair pair) {
+        boolean found = false;
+        for (int j = 0; j < circuits.size(); j++) {
+            var left = circuits.get(j);
+            var aInLeft = left.contains(pair.a());
+            var bInLeft = left.contains(pair.b());
+            if (aInLeft && bInLeft) {
+                found = true;
+                break;
+            }
+            if (aInLeft || bInLeft) {
+                for (int k = j + 1; k < circuits.size(); k++) {
+                    var right = circuits.get(k);
+                    var aInRight = right.contains(pair.a());
+                    var bInRight = right.contains(pair.b());
+                    if (aInRight || bInRight) {
+                        found = true;
+                        left.addAll(right);
+                        circuits.remove(k);
+                        break;
+                    }
+                }
+                if (!found) {
+                    left.add(aInLeft ? pair.b() : pair.a());
+                    found = true;
+                }
+                break;
+            }
+        }
+        if (!found) {
+            var newCircuit = new HashSet<Box>();
+            newCircuit.add(pair.a());
+            newCircuit.add(pair.b());
+            circuits.add(newCircuit);
+        }
     }
 }
 
